ace89769204ff58966976f59a2dd07837d08ab63
Ednan <52186220+ednanf@users.noreply.github.com>
2023-04-29 14:41:02 -0300

    Part 1 and 2 - completed.

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// MARK: - IBOutlets
+    @IBOutlet var topSegmentedControl: UISegmentedControl!
+    @IBOutlet var bottomSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var topCaptionLabel: UILabel!
+    @IBOutlet var bottomeCaptionLabel: UILabel!
+    
+// MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+// MARK: - IBActions
 
+    @IBAction func segmentedControls(_ sender: Any) {
+    }
+    
+// MARK: - Functions
+    
+    
+    
 }
 
